036a4f6f620a872bf3c35c00706e4bd7663d0e03
houyahui1 <houyahui1@guazi.com>
2022-06-24 19:22:29 +0800

    添加不同的上线文启动

--- spring-framework/src/main/java/com/yahui/SpringIcoDemo1.java
+++ spring-framework/src/main/java/com/yahui/SpringIcoDemo1.java
@@ -1,9 +1,11 @@
 package com.yahui;
 
 import com.yahui.bean.springicodemo.UserDto;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.AbstractXmlApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.GenericApplicationContext;
 
 /**
  * @author yahui
@@ -15,8 +17,21 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class SpringIcoDemo1 {
 
     public static void main(String[] args) {
-        ApplicationContext context = new ClassPathXmlApplicationContext("UserConfig.xml");
-        UserDto userDto = context.getBean("user",UserDto.class);
+        UserDto userDto = getGenericApplicationContext(UserDto.class);
         System.out.println(userDto.getAge()+userDto.getName());
+    }
+
+    //由于ClassPathXmlApplicationContext继承自AbstractApplicationContext，并且在new的时候执行了refresh，所以不需要显示的调用这个方法
+    private static UserDto getApplicationContextBean(Class<UserDto> userDtoClass){
+        ApplicationContext context = new ClassPathXmlApplicationContext("UserConfig.xml");
+        return context.getBean("user",userDtoClass);
+    }
+
+    //GenericApplicationContext需要显示的调用refresh
+    private static UserDto getGenericApplicationContext(Class<UserDto> userDtoClass){
+        GenericApplicationContext genericApplicationContext = new GenericApplicationContext();
+        new XmlBeanDefinitionReader(genericApplicationContext).loadBeanDefinitions("UserConfig.xml");
+        genericApplicationContext.refresh();
+        return genericApplicationContext.getBean("user",userDtoClass);
     }
 }
